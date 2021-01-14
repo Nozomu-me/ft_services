3f397fdd3097340eed4d2fbba9f4bdc0a295b650
Amal Ouassit <amouassi@e1r10p5.1337.ma>
2021-01-14 19:42:53 +0100

    php

--- srcs/wordpress/wp-config-sample.php
+++ srcs/wordpress/wp-config-sample.php
@@ -29,7 +29,7 @@ define( 'DB_USER', 'amal' );
 define( 'DB_PASSWORD', 'amal' );
 
 /** MySQL hostname */
-define( 'DB_HOST', 'mysql' );
+define( 'DB_HOST', 'localhost' );
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
