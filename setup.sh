52a7cfa00282f0053dc6e63783bcb6f25f2b60ee
Amal Ouassit <amouassi@e1r10p5.1337.ma>
2021-01-15 19:26:14 +0100

    mysql wordpress yaml

---
+++ setup.sh
@@ -0,0 +1,10 @@
+
+# docker build -t nginx srcs/nginx 
+# docker build -t phpmyadmin srcs/phpmyadmin
+docker build -t wordpress srcs/wordpress
+docker build -t mysql srcs/mysql
+
+# kubectl apply -f srcs/nginx/nginx.yaml 
+# kubectl apply -f srcs/phpmyadmin/phpmyadmin.yaml 
+kubectl apply -f srcs/wordpress/wordpress.yaml 
+kubectl apply -f srcs/mysql/mysql.yaml 
